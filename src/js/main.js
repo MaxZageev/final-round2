a84135a30fbedc568c8af8db1bd59249d8b6b463
MaxZageev <max.ageev2002@outlook.com>
2025-05-20 18:59:15 +0300

    added text size button

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
 import '../../node_modules/focus-visible/dist/focus-visible';
 import '../scss/main.scss';
 import '../index.html';
+
+// кнопка скрыть/показать
+const ReadMoreBtn = document.querySelector('.button_read-more');
+const ArticleTextContainer = document.querySelector('.article__text--size');
+ReadMoreBtn.addEventListener('click', () => {
+    ArticleTextContainer.classList.toggle('article__text--less');
+    ReadMoreBtn.classList.toggle('button_read-less');
+    const ReadLessBth = document.querySelector('.button_read-less')
+    if (ReadLessBth){
+    ReadLessBth.textContent = 'Скрыть';
+    } else {
+    ReadMoreBtn.textContent = 'Читать далее';
+    }
+});
